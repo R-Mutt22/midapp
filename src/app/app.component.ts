4a8e78e15a356f6c12c28f5354f48c2c6cd23d08
matias zelarayan <matiaszelarayan87@gmail.com>
2024-02-15 18:04:22 -0300

    feat: integrar wallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'midapp-root',
-  template: '
-    <header>
-    <h1> Hola RMutt </h1>
-    </header>
-  ',
+  template: ` <header class="py-8">
+    <h1 class="text-5xl text-center mb-4">Hola, R Mutt.</h1>
+
+    <div class="flex justify-center">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+    </div>
+  </header>`,
 })
-export class AppComponent {
-}
+export class AppComponent {}
